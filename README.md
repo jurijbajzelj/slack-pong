5901e5e1954c1954195774d76137444d0563e4fe
Jurij Bajzelj <jurij.bajzelj@xapo.com>
2019-10-14 20:26:38 +0200

    initial move to testing db relates logic

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # slack-pong
 https://ngrok.com/
+docker run -e POSTGRES_USER=root -e POSTGRES_DB=circle-test_test -d --rm -p 5432:5432 circleci/postgres
--- test_app.py
+++ test_app.py
@@ -1,21 +1,25 @@
 import requests
+from models import Match
+from models import Base
 
 
 BASE_URL = 'http://localhost:5000'
 
 
-def test_authorization(test_server, monkeypatch):
-    with monkeypatch.context() as m:
-        m.setenv('SLACK_APP_PONG_SIGNING_SECRET', '0'*66)
-        raw_data = b'token=123&team_id=456&team_domain=abc&channel_id=789&channel_name=ch_name&user_id=ABC'
-        headers = {
-            'X-Slack-Signature': 'v0=1a2eeb0ac2a8a562cc98047a50bef95639047e7d2aeb54cb5a88a40617dedb57',
-            'X-Slack-Request-Timestamp': '1571051763'
-        }
-        response = requests.post(f'{BASE_URL}/won', data=raw_data, headers=headers)
-        print(response.text)
-        assert requests.post(f'{BASE_URL}/won', data=raw_data, headers=headers).status_code == 400  # bad request
+def test_authorization(test_server):
+    raw_data = b'token=123&team_id=456&team_domain=abc&channel_id=789&channel_name=ch_name&user_id=ABC'
+    headers = {
+        'X-Slack-Signature': 'v0=1a2eeb0ac2a8a562cc98047a50bef95639047e7d2aeb54cb5a88a40617dedb57',
+        'X-Slack-Request-Timestamp': '1571051763'
+    }
+    response = requests.post(f'{BASE_URL}/won', data=raw_data, headers=headers)
+    print(response.text)
+    assert requests.post(f'{BASE_URL}/won', data=raw_data, headers=headers).status_code == 400  # bad request
 
-        # change last digit of a timestamp
-        headers.update({'X-Slack-Request-Timestamp': '1571051764'})
-        assert requests.post(f'{BASE_URL}/won', data=raw_data, headers=headers).status_code == 401  # unauthorized
+    # change last digit of a timestamp
+    headers.update({'X-Slack-Request-Timestamp': '1571051764'})
+    assert requests.post(f'{BASE_URL}/won', data=raw_data, headers=headers).status_code == 401  # unauthorized
+
+
+def test_db(prepare_db, db_session, test_server, monkeypatch):
+    assert db_session.query(Match).count() == 0
